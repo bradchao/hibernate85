55d44e91567273171e20d5bf281c08e9e199623f
User <User@10.0.100.157>
2024-09-09 12:04:01 +0800

    09091203

--- hi2/src/main/java/tw/brad/hi2/model/Bike.java
+++ hi2/src/main/java/tw/brad/hi2/model/Bike.java
@@ -2,9 +2,12 @@ package tw.brad.hi2.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,8 +18,9 @@ public class Bike {
 	@Column(name = "bid")
 	private int bid;
 	
-	@Column(name = "uid")
-	private int uid;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "uid")
+	private User user;
 	
 	@Column(name = "color")
 	private String color;
@@ -30,14 +34,6 @@ public class Bike {
 
 	public void setBid(int bid) {
 		this.bid = bid;
-	}
-
-	public int getUid() {
-		return uid;
-	}
-
-	public void setUid(int uid) {
-		this.uid = uid;
 	}
 
 	public String getColor() {
@@ -54,6 +50,14 @@ public class Bike {
 
 	public void setSpeed(double speed) {
 		this.speed = speed;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 	
 	
--- hi2/src/main/java/tw/brad/hi2/model/User.java
+++ hi2/src/main/java/tw/brad/hi2/model/User.java
@@ -1,10 +1,16 @@
 package tw.brad.hi2.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,7 +23,14 @@ public class User {
 	
 	@Column(name = "name")
 	private String name;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+	private List<Bike> bikes;
 
+	public User() {
+		bikes = new LinkedList<Bike>();
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -33,9 +46,22 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public List<Bike> getBikes() {
+		return bikes;
+	}
+
+	public void setBikes(List<Bike> bikes) {
+		this.bikes = bikes;
+	}
 	
+	public void addBike(Bike bike) {
+		bikes.add(bike);
+	}
 	
-	
+	public void removeBike(Bike bike) {
+		bikes.remove(bike);
+	}
 	
 	
 }
